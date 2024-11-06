aef4b67a708cf993e89385a7fb2bdb5d8d828bca
Carolina Bailey <cmonjarr@gmail.com>
2024-11-05 19:01:48 -0600

    Added caesar-cipher code

--- cipher.py
+++ cipher.py
@@ -1,1 +1,24 @@
 # add your code here
+letters = "abcdefghijklmnopqrstuvwxyz"
+
+encryption = {}
+for i in range(26):
+    j = (i + 5) % 26
+    encryption[letters[i]] = letters[j]
+
+#print(encryption)
+#for letter in letters:
+#    print(letter, encryption[letter])
+
+user_sentence = input("Please enter a sentence:")
+print(user_sentence)
+user_sentece = user_sentence.lower()
+print("Plain sentence:", user_sentence)
+
+encrypted_text = ""
+for letter in user_sentece:
+    if letter in encryption:
+        encrypted_text += encryption[letter]
+    else:
+        encrypted_text += letter
+print("The encrypted sentence is:",  encrypted_text)
